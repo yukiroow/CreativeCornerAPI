022ce8a43252eec820fb2e7d326c9944ec053338
Harry Dominguez Jr. <hcdominguez07@gmail.com>
2024-05-26 19:03:09 +0800

    Removed everything. Reorganization

--- src/main/java/org/cc/creativecornerapi/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.cc.creativecornerapi.repository;
-
-import org.cc.creativecornerapi.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer>, JpaSpecificationExecutor<User>, CrudRepository<User,Integer> {
-}
